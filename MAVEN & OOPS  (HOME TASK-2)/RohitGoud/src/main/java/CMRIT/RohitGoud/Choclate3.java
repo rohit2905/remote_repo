4c7cebe1c307a84334b596d257a3e252839a5654
rohit2905 <krohitgoud@gmail.com>
2020-02-02 20:43:48 +0530

    HOME TASK-2

---
+++ MAVEN & OOPS  (HOME TASK-2)/RohitGoud/src/main/java/CMRIT/RohitGoud/Choclate3.java
@@ -0,0 +1,13 @@
+package CMRIT.RohitGoud;
+
+public class Choclate3 extends NewYearChoclates {
+
+	@Override
+	public int weightOfGift(int wt) {
+		totalweight+=wt;
+		return totalweight;
+		
+	}
+
+	
+}
---
+++ MAVEN & OOPS  (HOME TASK-2)/RohitGoud/src/main/java/CMRIT/RohitGoud/Sweet1.java
@@ -0,0 +1,13 @@
+package CMRIT.RohitGoud;
+
+public class Sweet1 extends NewYearChoclates {
+
+	@Override
+	public int weightOfGift(int wt) {
+		totalweight+=wt;
+		return totalweight;
+		
+	}
+
+	
+}
